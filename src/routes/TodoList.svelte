b860c62cbb63f781277910ed659684e8149bfcd3
elidvenega <robotsmailNinja@gmail.com>
2024-04-17 23:00:03 -0700

    Updated

--- src/routes/TodoList.svelte
+++ src/routes/TodoList.svelte
@@ -2,22 +2,10 @@
   let newItem = "";
 
   let todoList = [
-    {
-      task: "Go hiking",
-      status: false,
-    },
-    {
-      task: "Get groceries",
-      status: false,
-    },
-    {
-      task: "Washing",
-      status: false,
-    },
-    {
-      task: "Study",
-      status: false,
-    },
+    { task: "Go Hiking", status: false },
+    { task: "Groceries", status: false },
+    { task: "Washing", status: false },
+    { task: "Study", status: false },
   ];
 
   function addTodo() {
@@ -25,7 +13,7 @@
     newItem = "";
   }
 
-  function deleteTodo() {
+  function deleteTodo(index) {
     todoList.splice(index, 1);
     todoList = todoList;
   }
@@ -33,23 +21,30 @@
 
 <h1>Svelte Todo List</h1>
 <div class="todo-list-container">
-  <input bind:value={newItem} class="input" placeholder="add todo" type="text" name="todo-list" />
+  <input
+    bind:value={newItem}
+    class="input"
+    placeholder="Add todo"
+    type="text"
+    name="todo-list"
+  />
   <button class="add-todo-button" on:click={addTodo}>Add Todo</button>
   <br />
   {#each todoList as item, index}
-  <input bind:checked={item.status} type="checkbox" />
-  <span class:checked={item.status}>{item.task}</span>
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <!-- svelte-ignore a11y-no-static-element-interactions -->
-  <span on:click={() => deleteTodo(index)}>❌</span>
-{/each}
+    <div>
+      <input bind:checked={item.status} type="checkbox" />
+      <span class:checked={item.status}>{item.task}</span>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <!-- svelte-ignore a11y-no-static-element-interactions -->
+      <span class="delete-button" on:click={() => deleteTodo(index)}>❌</span>
+    </div>
+  {/each}
 </div>
-
-
 
 <style>
   :root {
     --color-one: #f97316;
+    --white-color: #fff;
     --font-size: 1rem;
   }
 
@@ -65,20 +60,32 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+    margin: 0 auto;
     margin-top: 2rem;
     gap: 5px;
     width: 30rem;
   }
 
+  input {
+    padding: 10px;
+  }
+
   .checked {
     text-decoration: line-through;
   }
-  /* 
-  input {
-    width: 12rem;
+
+  .delete-button {
+    cursor: pointer;
   }
 
   .add-todo-button {
+    background-color: var(--color-one);
+    border: none;
+    color: var(--white-color);
+    padding: 10px;
+  }
+
+  /* .add-todo-button {
     width: 4rem;
   } */
 </style>
